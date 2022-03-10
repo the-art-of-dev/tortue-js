184fd3b9db06751ca35e64142d790e437c9ea2d5
Vukoman Stojanovic <vukomans96@yahoo.com>
2022-03-10 03:30:44 +0100

    Add npm scripts

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ["<rootDir>/src", "test"],
+  roots: ["<rootDir>/src", "tests"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
