327c13d2aace5207ffed2d7c1f3f3c64fcb4c09f
Vukoman Stojanovic <vukomans96@yahoo.com>
2022-03-10 03:26:12 +0100

    Add rollup to project

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist",
+      format: "cjs",
+    },
+    plugins: [typescript(), commonjs()],
+  },
+];
