b6c4b5e357390bce39f97242a55a000873642f4f
Vukoman Stojanovic <vukomans96@yahoo.com>
2022-03-22 22:31:09 +0100

    Add Component Builder tests

--- tests/lib/components/componentBuilder.test.ts
+++ tests/lib/components/componentBuilder.test.ts
@@ -1,3 +1,26 @@
+import { ComponentBuilder } from "@lib/components";
+import { Context } from "@lib/contexts";
+import path from "path";
+
 describe("Component Builder", () => {
-  it("Build All", () => {});
+  it("Build All/Contexts", async () => {
+    const builder = new ComponentBuilder(
+      path.resolve("testData", "components"),
+    );
+
+    const contexts = await builder.getComponentsContexts();
+    expect(contexts.map((c) => c.name)).toEqual([
+      "Common-Footer",
+      "Common-Header",
+      "Home-Section",
+      "Home-Slider",
+    ]);
+
+    const registry = await builder.buildAllComponents();
+    expect(registry.getAllComponents().map((c) => c.name)).toEqual([
+      "Common-Header",
+      "Home-Section",
+      "Home-Slider",
+    ]);
+  });
 });
